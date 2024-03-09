0935acc4ac0d4c9ebc3e65fb52faef61bf1a4a6d
Caroline-An <anacarolinesn@outlook.com>
2024-03-09 16:38:55 -0300

    iniciando projeto

---
+++ codigo.py
@@ -0,0 +1,17 @@
+#Título: ChatHoi
+#Botão de iniciar chat
+    #clicou no botão:
+        #popup/modal
+            #Título: Bem vindo/a ao ChatHoi
+            #Campo: escreva seu nome
+            #botão: entrar
+
+#Chat
+#Abaixo do chat:
+    #campo de digitar mensagem
+    #botão enviar
+
+
+#pip install flet -> no terminal
+import fileinput as ft
+
